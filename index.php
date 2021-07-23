afb4d3eb2eda780f7c55a087a5e22985891aa4be
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-07-23 17:02:04 +0100

    breaks_and_continue

--- index.php
+++ index.php
@@ -2,36 +2,6 @@
 
 $name= "Sphinx PHP";
 
-//comparison booleans 
-// echo true;
-// echo 'gypsy' < 'titan';
-// echo 'gypsy' > 'Gypsy';
-// echo 'gypsy' == 'Gypsy';
-
-
-//loose vs strict equal comparison
-//loose comparison does not take the data type into consideration
-//echo 14 == '14'; //returns true
-//echo 14 === '14';
-
-//echo true == '1';
-//echo false == '';
-
-//conditional statements
-
-// $price = 90;
-
-// if ($price > 800){
-//     echo 'the condition is met';
-// }elseif( $price = 90){
-//     echo 'else if condition';
-// }
-
-// else {
-//     echo 'Oops not exist';
-// }
-
-
 $products = [
     ['name' => 'shiny star', 'price' => 20],
     ['name' => 'green shell', 'price' => 10],
@@ -41,16 +11,18 @@ $products = [
     ['name' => 'banana skin', 'price' => 2]
 ];
 
-// foreach($products as $product){
-//     if($product['price'] < 15 && $product['price'] > 2 ){
-//         echo $product['name'] . '<br/>';
-//     }
-// }
-// foreach($products as $product){
-//     if($product['price'] > 20 || $product['price'] <10  ){
-//         echo $product['name'] . '<br/>';
-//     }
-// }
+foreach($products as $product){
+    if($product['name']=== 'gold coin'){
+        break;
+    }
+    
+    if($product['price'] > 15){
+        continue;
+    }
+    echo $product['name'] . '<br/>';
+    echo $product['price'] . '<br/>';
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -66,14 +38,6 @@ $products = [
         <?php echo $name; ?>
         
     </h1>
-    <div>
-        <ul>
-            <?php forEach($products as $product){ ?>
-                <?php if($product['price'] > 15 ) { ?>
-                    <li> <?php echo $product['name']; ?> </li>
-                    <?php }?>
-                <?php }?>
-        </ul>
-    </div>
+   
 </body>
 </html>
