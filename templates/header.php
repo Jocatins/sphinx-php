b166d64d5e248bd073928b2de8feadc992377394
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-07-25 18:55:06 +0100

    cookie

--- templates/header.php
+++ templates/header.php
@@ -12,8 +12,10 @@ if ($_SERVER['QUERY_STRING'] == 'noname') {
 // Null Coalescing
 
 $name = $_SESSION['name'] ?? 'Guest';
-?>
 
+// get cookie
+$gender = $_COOKIE['gender'] ?? 'Unknown';
+?>
 
 <head>
 	<title>Titan Pizzas</title>
@@ -50,6 +52,7 @@ $name = $_SESSION['name'] ?? 'Guest';
 			<a href="index.php" class="brand-logo brand-text">Titan Pizzas</a>
 			<ul id="nav-mobile" class="right hide-on-small-and-down">
 				<li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+				<li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
 				<li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
 			</ul>
 		</div>
