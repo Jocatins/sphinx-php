9e61e09e86e69d650cdae892fca5bbe3d7f93fc6
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-07-25 07:01:47 +0100

    explodeFunc

--- index.php
+++ index.php
@@ -1,21 +1,67 @@
-<?php 
-
+<?php
 
 // connect to database
 $conn = mysqli_connect('localhost', 'sphinx', '$phinx@007', 'titan-pizza');
 
 //check connection
-if (!$conn){
-    echo 'Connection unsuccessful'.  mysqli_connect_error();
+if (!$conn) {
+    echo 'Connection unsuccessful' .  mysqli_connect_error();
 }
+//write query to get all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY createdAt';
+
+//make query & get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free result from memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($conn);
+
+//explode function
+//print_r(explode(' ', $pizzas[0]['ingredients']));
+
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
-	
-	<?php include('templates/header.php'); ?>
 
-	<?php include('templates/footer.php'); ?>
+<?php include('templates/header.php'); ?>
+
+
+<h4 class="center grey-text">Titan Pizzas!</h4>
+
+<div class="container">
+  <div class="row">
+  <?php foreach ($pizzas as $pizza) { ?>
+
+    <div class="col s6 md3">
+      <div class="card z-depth-0">
+          <div class="card-content center">
+              <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+             <ul>
+                 <?php foreach(explode(' ', $pizza['ingredients']) as $ing){ ?>
+                    <li><?php echo htmlspecialchars($ing)?> </li>
+              <?php   } ?>
+             </ul>
+          </div>
+          <div class="card-action right-align">
+              <a class="brand-text" href="#">more info</a>
+          </div>
+      </div>
+    </div>
+
+  <?php } ?>
+
+  </div>
+</div>
+
+<?php include('templates/footer.php'); ?>
 
 </html>
