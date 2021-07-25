b9004c76b5a5db9a7e1bb2f97b5cbb28081224c6
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-07-25 14:28:12 +0100

    delete4rmDb

--- index.php
+++ index.php
@@ -32,34 +32,34 @@ mysqli_close($conn);
 <h4 class="center grey-text">Titan Pizzas!</h4>
 
 <div class="container">
-  <div class="row">
-  <?php foreach ($pizzas as $pizza) : ?>
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) : ?>
 
-    <div class="col s6 md3">
-      <div class="card z-depth-0">
-          <div class="card-content center">
-              <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-             <ul>
-                 <?php foreach(explode(' ', $pizza['ingredients']) as $ing): ?>
-                    <li><?php echo htmlspecialchars($ing)?> </li>
-              <?php   endforeach; ?>
-             </ul>
-          </div>
-          <div class="card-action right-align">
-              <a class="brand-text" href="#">more info</a>
-          </div>
-      </div>
-    </div>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <ul>
+                            <?php foreach (explode(' ', $pizza['ingredients']) as $ing) : ?>
+                                <li><?php echo htmlspecialchars($ing) ?> </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                    <div class="card-action right-align">
+                        <a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
+                    </div>
+                </div>
+            </div>
 
-  <?php endforeach; ?>
+        <?php endforeach; ?>
 
-  <?php if(count($pizzas) >=3): ?>
-    <p>There are 3 more pizzas</p>
+        <?php if (count($pizzas) >= 3) : ?>
+            <p>There are 3 more pizzas</p>
 
-    <?php  else :?>
-        <p>There are less than 3</p>
+        <?php else : ?>
+            <p>There are less than 3</p>
         <?php endif; ?>
-  </div>
+    </div>
 </div>
 
 <?php include('templates/footer.php'); ?>
